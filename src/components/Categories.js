4ccaa38f6c9f90bbd95c7e3a0c0c02e5895843d7
MCris29 <crismax0629@gmail.com>
2021-03-24 19:39:03 -0500

    Carousel Categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,7 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
@@ -12,50 +10,54 @@ import useSWR from "swr";
 import {fetcher} from "@/lib/utils";
 import Loading from "@/components/Loading";
 import Routes from "@/constants/routes";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 const useStyles = makeStyles((theme) => ({
-    icon: {
-        marginRight: theme.spacing(2),
-    },
-    heroContent: {
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(8, 0, 6),
-    },
+    title: {
+        color: "#fff",
+        paddingBottom: "30px",
 
+    },
     cardGrid: {
         maxWidth: "100%",
         backgroundColor: "#44857D",
+        margin: "20px 0",
+        padding: "30px 0",
     },
     cardContainer: {
-        backgroundColor: "#4485D",
+        margin: "0 20px",
     },
-    card: {
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        backgroundColor: "#cfd3d6",
-        borderColor: "none",
-        boxShadow: "none",
+    root: {
+        maxWidth: 345,
     },
-    cardMedia: {
-        paddingTop: "56.25%", // 16:9
-        borderRadius: "10px",
-        height: "100%",
-    },
-
-    avatar: {
-        backgroundColor: "#FE6625",
-        color: "#FFFFFF",
-    },
-    actions: {
-        flexGrow: 2,
-    },
-    cardavatar: {
-        position: "relative",
-
-        zIndex: "modal",
+    media: {
+        height: 140,
     },
 }));
+
+const responsive = {
+    superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: {max: 4000, min: 3000},
+        items: 5
+    },
+    desktop: {
+        breakpoint: {max: 3000, min: 1024},
+        items: 3
+    },
+    tablet: {
+        breakpoint: {max: 1024, min: 464},
+        items: 2
+    },
+    mobile: {
+        breakpoint: {max: 464, min: 0},
+        items: 1
+    }
+};
 
 const Categories = () => {
     const classes = useStyles();
@@ -65,7 +67,7 @@ const Categories = () => {
     if (!data)
         return (
             <div>
-                <Loading/>
+                <Skeleton variant="rect" width={210} height={118}/>
             </div>
         );
 
@@ -77,57 +79,41 @@ const Categories = () => {
                 className={classes.cardGrid}
                 maxWidth="md"
             >
-                <Typography
-                    component="h6"
-                    variant="h4"
-                    style={{color: "#fff", paddingTop: "30px"}}
-                >
-                    Categorias
-                </Typography>
-            </Container>
-            <Container
-                className={classes.cardGrid}
-                maxWidth="md"
-            >
-                <Grid container spacing={5}>
-                    {
-                        data.data.map((data) => (
-                            <Grid
-                                item
-                                key={data.id}
-                                xs={12} sm={4} md={3}
-                                className={classes.cardContainer}
-                            >
-                                <Card
-                                    className={classes.card}
-                                    border={0}
-                                    style={{
-                                        backgroundColor: "#44857D",
-                                    }}
-                                >
-                                    <Button
-                                        variant="outlined"
-                                        style={{
-                                            position: "relative",
-                                            top: "50%",
-                                            zIndex: "modal",
-                                            backdropFilter: "blur(6px)",
-                                        }}
-                                    >
-                                        <Link href={`${Routes.CATEGORY}/${data.id}`}>
-                                            {data.name}
-                                        </Link>
-                                    </Button>
-                                    <CardMedia
-                                        className={classes.cardMedia}
-                                        image={`https://picsum.photos/200/300?sig=${data.id}`}
-                                        title="Image title"
-                                        style={{align: "center", borderColor: "#44857D"}}
-                                    />
-                                </Card>
-                            </Grid>
-                        ))}
-                </Grid>
+                <Container item xs={12}>
+                    <Typography
+                        component="h6"
+                        variant="h4"
+                        className={classes.title}
+                    >
+                        Categorias
+                    </Typography>
+                </Container>
+                <Container item xs={12}>
+                    <Carousel responsive={responsive}>
+                        {
+                            data.data.map((data) => (
+                                <div key={data.id} className={classes.cardContainer}>
+                                    <Link href={`${Routes.CATEGORY}/${data.id}`}>
+                                        <Card className={classes.root}>
+                                            <CardActionArea>
+                                                <CardMedia
+                                                    className={classes.media}
+                                                    image={`https://picsum.photos/200/300?sig=${data.id}`}
+                                                />
+                                                <CardContent>
+                                                    <Typography gutterBottom variant="h5" component="h3">
+                                                        {data.name}
+                                                    </Typography>
+                                                </CardContent>
+                                            </CardActionArea>
+                                        </Card>
+
+                                    </Link>
+
+                                </div>
+                            ))}
+                    </Carousel>
+                </Container>
             </Container>
         </>
     );
